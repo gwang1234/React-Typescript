b1140ede110d345b3765fb53a5878d3e011a47ba
qoi11 <qoi11@naver.com>
2026-01-06 08:31:11 +0900

    jsx 투두리스트

--- jsx_section08/src/components/Editor.jsx
+++ jsx_section08/src/components/Editor.jsx
@@ -1,9 +1,36 @@
+import { useState, useRef } from "react"
 import "./Editor.css"
 
-const Editor = () => {
+const Editor = ({onCreate}) => {
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value)
+    }
+
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            onSubmit();
+        }
+    }
+
+    const onSubmit =() => {
+        if (content === "") {
+            contentRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    }
+
     return <div className="Editor">
-        <input placeholder="새로운 Todo..."/>
-        <button>추가</button>
+        <input value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeyDown}
+        ref={contentRef}
+        placeholder="새로운 Todo..."/>
+        <button onClick={onSubmit}>추가</button>
     </div>
 }
 
