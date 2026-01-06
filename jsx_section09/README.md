38028c92757b8cc727d08f784ff3126a1cda99a0
gwang1234 <qoi11@naver.com>
2026-01-06 17:01:24 +0900

    jsx useReducer

---
+++ jsx_section09/README.md
@@ -0,0 +1,6 @@
+# useReducer
+
+- 컴포넌트 내부에 새로운 STate를 생성하는React Hook
+- 모든 useState는 useReducer로 대체 가능
+- useState와 달리 상태 관리 코드를 컴포넌트 외부로 분리할 수 있음
+  - 상태관리 코드를 분리하여 가독성을 높여줌
---
+++ jsx_section09/src/components/Editor.jsx
@@ -0,0 +1,37 @@
+import { useState, useRef } from "react"
+import "./Editor.css"
+
+const Editor = ({onCreate}) => {
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value)
+    }
+
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            onSubmit();
+        }
+    }
+
+    const onSubmit =() => {
+        if (content === "") {
+            contentRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    }
+
+    return <div className="Editor">
+        <input value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeyDown}
+        ref={contentRef}
+        placeholder="새로운 Todo..."/>
+        <button onClick={onSubmit}>추가</button>
+    </div>
+}
+
+export default Editor;
